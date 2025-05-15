ffe6412366323346d145633037018c247020926b
Trần Gia Huy <huytgcs170073@fpt.edu.vn>
2025-05-15 13:30:17 +0700

    add folder  seller

--- Food_Haven.Web/Controllers/SellerController.cs
+++ Food_Haven.Web/Controllers/SellerController.cs
@@ -1,12 +1,114 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Net.Http.Headers;
+using AutoMapper;
+using BusinessLogic.Services.BalanceChanges;
+using BusinessLogic.Services.OrderDetailService;
+using BusinessLogic.Services.Orders;
+using BusinessLogic.Services.Products;
+using BusinessLogic.Services.ProductVariants;
+using BusinessLogic.Services.Reviews;
+using BusinessLogic.Services.StoreDetail;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Models;
+using Repository.StoreDetails;
+using Repository.ViewModels;
 
 namespace Food_Haven.Web.Controllers
 {
     public class SellerController : Controller
     {
+        private readonly IReviewService _reviewService;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly IProductService _productService;
+        private readonly IStoreDetailService _storeDetailService;
+        private HttpClient client = null;
+        private string _url;
+
+        private readonly StoreDetailsRepository _storeRepository;
+        private readonly IMapper _mapper;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        private readonly IProductVariantService _variantService;
+        private readonly IOrdersServices _order;
+        private readonly IBalanceChangeService _balance;
+        private readonly IOrderDetailService _orderDetail;
+
+        public SellerController(IReviewService reviewService, UserManager<AppUser> userManager, IProductService productService, IStoreDetailService storeDetailService, StoreDetailsRepository storeRepository, IMapper mapper, IWebHostEnvironment webHostEnvironment, IProductVariantService variantService, IOrdersServices order, IBalanceChangeService balance, IOrderDetailService orderDetail)
+        {
+            _reviewService = reviewService;
+            _userManager = userManager;
+            _productService = productService;
+            _storeDetailService = storeDetailService;
+            client = new HttpClient();
+            var contentype = new MediaTypeWithQualityHeaderValue("application/json");
+            client.DefaultRequestHeaders.Accept.Add(contentype);
+            _storeRepository = storeRepository;
+            _mapper = mapper;
+            _webHostEnvironment = webHostEnvironment;
+            _variantService = variantService;
+            _order = order;
+            _balance = balance;
+            _orderDetail = orderDetail;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
+        public async Task<IActionResult> FeedbackList()
+        {
+            var result = new List<ReivewViewModel>();
+
+            try
+            {
+                var user = await _userManager.GetUserAsync(User);
+                if (user == null)
+                {
+                    return RedirectToAction("Login", "Home");
+                }
+
+                var storeDetail = await _storeDetailService.FindAsync(s => s.UserID == user.Id);
+                if (storeDetail == null)
+                {
+                    return View(result);
+                }
+
+                var storeId = storeDetail.ID;
+
+                // Lấy danh sách sản phẩm theo StoreID
+                var products = await _productService.ListAsync(p => p.StoreID == storeId);
+                var productIds = products.Select(p => p.ID).ToList();
+
+                // Lấy các review thuộc những sản phẩm này
+                var reviews = await _reviewService.ListAsync(r => productIds.Contains(r.ProductID));
+
+                foreach (var review in reviews)
+                {
+                    var reviewer = await _userManager.FindByIdAsync(review.UserID);
+                    var product = products.FirstOrDefault(p => p.ID == review.ProductID);
+
+                    var reviewModel = new ReivewViewModel
+                    {
+                        ID = review.ID,
+                        Comment = review.Comment,
+                        CommentDate = review.CommentDate,
+                        Reply = review.Reply,
+                        Status = review.Status,
+                        Rating = review.Rating,
+                        Username = reviewer?.UserName,
+                        ProductName = product?.Name,
+                        StoreId = storeId
+                    };
+
+                    result.Add(reviewModel);
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new { status = "error", msg = "There was an error loading Feedback List." });
+            }
+
+            return View(result);
+        }
+
     }
 }
