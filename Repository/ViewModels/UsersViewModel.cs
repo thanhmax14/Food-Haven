f790144325a6b4b6c2f1c82ebeaee22763f8eac7
Le Duc Anh-N1 <AnhLDCE171348@fpt.edu.vn>
2025-07-21 22:24:49 +0700

    LeDucAnh_FixCode_V4
    LeDucAnh_FixCode_V4

--- Repository/ViewModels/UsersViewModel.cs
+++ Repository/ViewModels/UsersViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,15 +9,16 @@ namespace Repository.ViewModels
 {
     public class UsersViewModel
     {
-    /*  public DateTime? joinin { get; set; } = DateTime.Now;
-        public DateTime lastAssces { get; set; } = DateTime.Now; */
+        /*  public DateTime? joinin { get; set; } = DateTime.Now;
+            public DateTime lastAssces { get; set; } = DateTime.Now; */
         public string? FirstName { get; set; } = default;
-        public string? LastName { get; set; } = default; 
+        public string? LastName { get; set; } = default;
         public DateTime? Birthday { get; set; }
         public string? Address { get; set; } = default;
         public string? RequestSeller { get; set; }
         public string? Email { get; set; }
         public string? UserName { get; set; }
+        public string? UserNameAdmin { get; set; }
         public string? PhoneNumber { get; set; }
         public string? img { get; set; } = "";
         public bool isUpdateProfile { get; set; } = false;
@@ -26,6 +28,8 @@ namespace Repository.ViewModels
         */
         public DateTime? ModifyUpdate { get; set; } = DateTime.Now;
         public Guid StoreDeatilId { get; set; }
+        [StringLength(250, ErrorMessage = "Reject note cannot exceed 250 characters.")]
+        public string? RejectNote { get; set; }
 
     }
 }
