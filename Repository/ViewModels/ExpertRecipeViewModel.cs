cc0378a7c69bd2ab6c709ebc50bc60542eb1f1a8
Pham Quang Thanh - N1 <thanhpqce171732@fpt.edu.vn>
2025-08-01 18:23:48 +0700

    a

---
+++ Repository/ViewModels/ExpertRecipeViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.ViewModels
+{
+    public class ExpertRecipeViewModel
+    {
+        public string title { get; set; }
+
+        public List<string> ingredients { get; set; }
+
+        public List<string> directions { get; set; }
+
+        public List<string> ner { get; set; }
+
+        public string link { get; set; }
+
+        public string source { get; set; }
+    }
+    public class ExpertRecipeEditViewModel
+    {
+        public Guid Id { get; set; }
+        public string title { get; set; }
+        public List<string> ingredients { get; set; }
+        public List<string> directions { get; set; }
+        public List<string> ner { get; set; }
+        public string link { get; set; }
+        public string source { get; set; }
+    }
+}
